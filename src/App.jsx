037d75c7c454780644f189b980860c114e297123
pravesh dav <humantorch1932@gmail.com>
2024-06-25 20:11:51 +0530

    added hero component and done mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header'
+import Hero from './Components/Hero'
 
 function App() {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import insta from "../assets/images/icon/insta.svg";
+import whatsapp from "../assets/images/icon/whatsapp.svg";
+import linkedIn from "../assets/images/icon/linkedIn.svg";
+import gitHub from "../assets/images/icon/gitHub.svg";
+import imageBg from "../assets/images/image-bg.svg";
+import myImage from "../assets/images/myImage.svg";
+import mouse from "../assets/images/icon/scrollMouse.svg";
+// import mouseArrow from "../assets/mouseArrow.svg";
+// import mouseBtn from "../assets/mouseBtn.svg";
+// import "./Hero.css";
+// import HeroDecoration from "./HeroDecoration";
+
+function Hero() {
+  let socialIcons = [
+    {
+      name: insta,
+    },
+    {
+      name: whatsapp,
+    },
+    {
+      name: linkedIn,
+    },
+    {
+      name: gitHub,
+    },
+  ];
+  return (
+    <div className="w-full h-screen">
+      {/* <HeroDecoration/> */}
+      <div id="hero-top" className="h-[70%] w-full flex relative">
+        <div className="w-[20%] h-[75%] flex flex-col gap-[0.6rem] items-center justify-end absolute left-0">
+          {socialIcons.map((item, index) => {
+            return (
+              <a key={index} href="#" className={`${index === 3 && "mb-8"}`}>
+                <img src={item.name} alt="" />
+              </a>
+            );
+          })}
+        </div>
+        <div className="w-full h-[80%] relative">
+          <div className="w-16 h-[1.4rem] flex justify-center items-center gap-1 absolute top-40 left-1/2 -translate-x-[215%] z-50 bg-white/30 rounded-[2.7px]">
+            <h3 className="text-white font-bold text-[0.5rem]">20+</h3>
+            <p className="text-white text-[0.35rem] tracking-widest leading-[0.4.3rem]">
+              Completed <br /> <span className="text-primary">Projects</span>
+            </p>
+          </div>
+          <div className="w-16 h-[1.4rem] flex justify-center items-center gap-1 absolute top-60 left-1/2 translate-x-[120%] z-50 bg-white/30 rounded-[2.7px]">
+            <h3 className="text-white font-bold text-[0.5rem]">1+</h3>
+            <p className="text-white text-[0.35rem] tracking-widest leading-[0.4.3rem]">
+              Years of <br /> <span className="text-primary">Experience</span>
+            </p>
+          </div>
+          <img
+            src={imageBg}
+            className="w-44 z-10 absolute bottom-0 left-1/2 -translate-x-1/2"
+            alt=""
+          />
+          <img
+            src={myImage}
+            className="w-52 z-20 absolute bottom-9 left-1/2 -translate-x-[48%]"
+            alt=""
+          />
+          <div className="w-44 h-24 rounded-b-[5.5rem] bg-gradient-to-t from-[#1a1a1a] from-50% to-transparent z-30 absolute bottom-0 left-1/2 -translate-x-1/2"></div>
+        </div>
+        <div className="w-full absolute bottom-7 left-1/2 -translate-x-1/2">
+          <h1 className=" text-center text-primary text-[2.1rem] font-monseret font-extrabold leading-7">
+            Pravesh <span className="stroke-text">Sa</span>ini
+          </h1>
+          <h2 className="w-full text-center uppercase text-white text-sm font-monseret font-bold">
+            frontend <span className="stroke-text">developer</span>
+          </h2>
+        </div>
+      </div>
+      <div
+        id="hero-bottom"
+        className="w-full h-[30%] flex justify-between pt-8 pl-11 pr-8 absolute"
+      >
+        <p className="w-28 text-[0.32rem] text-white/60 font-extralight text-justify mt-3">
+          Debbie put her hand into the hole, sliding her hand down as far as her
+          arm could reach. Lorem ipsum dolor sit amet Lorem, ipsum.
+        </p>
+        <div className="w-[7.2rem]">
+          <h3 className="text-[0.6rem] text-white -mb-2 mt-1">Email</h3>
+          <a
+            className="text-[0.5rem] text-white/50"
+            href="mailto:praveshsaini.dev@gmail.com"
+          >
+            praveshsaini.dev@gmail.com
+          </a>
+          <h3 className="text-[0.6rem] text-white -mb-2 mt-1">Phone</h3>
+          <a className="text-[0.5rem] text-white/50" href="tel:+9991831473">
+            9991831473
+          </a>
+          <h3 className="text-[0.6rem] text-white -mb-2 mt-1">Location</h3>
+          <p className="text-[0.5rem] text-white/50 mt-2">
+            123001 Niwaz Nagar <br /> Haryana India
+          </p>
+        </div>
+      </div>
+      <div className="w-full absolute bottom-1  flex flex-col justify-center items-center">
+        <span className="absolute top-1  rounded-3xl">
+          {/* <img className="w-[0.1rem]" src={mouseBtn} alt="" /> */}
+        </span>
+        <img className="w-[0.6rem]" src={mouse} alt="" />
+        {/* <img className="w-[0.6rem]" src={mouseArrow} alt="" /> */}
+        <p className="text-center text-white/50 font-thin text-[0.6rem] tracking-widest">
+          Scroll Down
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
