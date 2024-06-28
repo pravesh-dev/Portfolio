30dfd92df583719f615eb3eae57f5c9b96341bd8
pravesh dav <humantorch1932@gmail.com>
2024-06-28 16:15:12 +0530

    set circles and triangle locations in aboutdecoration little bit not completely

--- src/Components/AboutDecoration.jsx
+++ src/Components/AboutDecoration.jsx
@@ -5,22 +5,23 @@ function AboutDecoration() {
   return (
     <div className="w-full h-full absolute">
       {/* Small Circles */}
-      <span className="circle w-[0.3rem] h-[0.3rem] border border-[#A5A5A5] rounded-full absolute top-[13%] left-[30%] lg:w-[0.5rem] lg:h-[0.5rem] lg:left-[35%] lg:top-[20%]"></span>
+      <span className="circle w-[0.3rem] h-[0.3rem] border border-[#A5A5A5] rounded-full absolute top-[40%] left-[5%] lg:w-[0.5rem] lg:h-[0.5rem]"></span>
       <span className="circle w-[0.3rem] h-[0.3rem] border border-[#A5A5A5] rounded-full absolute top-[11%] left-[57%] lg:w-[0.5rem] lg:h-[0.5rem]"></span>
-      <span className="circle w-[0.3rem] h-[0.3rem] border border-[#A5A5A5] rounded-full absolute top-[25%] left-[80%] lg:w-[0.5rem] lg:h-[0.5rem] lg:left-[70%] lg:top-[30%]"></span>
+      <span className="circle w-[0.3rem] h-[0.3rem] border border-[#A5A5A5] rounded-full absolute top-[25%] left-[80%] lg:w-[0.5rem] lg:h-[0.5rem]"></span>
 
       {/* Large Circles */}
-      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[14%] left-[15%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
-      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[8%] left-[70%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
+      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[14%] left-[7%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
+      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[8%] left-[80%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
       <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[32%] left-[88%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
-      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[31%] left-[14%] lg:w-[0.8rem] lg:h-[0.8rem] lg:left-[24%] lg:top-[38%]"></span>
-      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[57%] left-[24%] lg:w-[0.8rem] lg:h-[0.8rem] lg:left-[34%]"></span>
+      
+      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[51%] left-[14%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
+      <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[57%] left-[24%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
       <span className="circle w-[0.6rem] h-[0.6rem] border border-[#A5A5A5] rounded-full absolute top-[53%] left-[77%] lg:w-[0.8rem] lg:h-[0.8rem]"></span>
 
       {/* Triangles */}
       <img
         src={triangle}
-        className="w-[0.6rem] absolute top-[16%] left-[68%]"
+        className="w-[0.6rem] absolute top-[4%] left-[44%] -rotate-[30deg]"
         alt="Triangle"
       />
       <img
