b7d9f3a33fcd328db30f72d837c5a19aaa2311f6
pravesh dav <humantorch1932@gmail.com>
2024-07-01 22:56:42 +0530

    added skill content and done styling little

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,8 +3,41 @@ import mouse from "../assets/images/icon/scrollMouse.svg";
 
 function Skills() {
   return (
-    <div className='w-full h-screen relative '>
-      
+    <div className='w-full h-screen relative flex justify-between items-center'>
+
+      <div className='w-32 border flex flex-col gap-5'>
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-white text-xl font-bold'>HTML</h2>
+          <p className='text-[0.6rem] text-white font-extralight tracking-widest leading-3 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse cum incidunt similique. Totam, aliquam.</p>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-white text-xl font-bold'>HTML</h2>
+          <p className='text-[0.6rem] text-white font-extralight tracking-widest leading-3 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse cum incidunt similique. Totam, aliquam.</p>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-white text-xl font-bold'>HTML</h2>
+          <p className='text-[0.6rem] text-white font-extralight tracking-widest leading-3 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse cum incidunt similique. Totam, aliquam.</p>
+        </div>
+      </div>
+
+    <h1 className='text-primary text-xl font-bold tracking-widest -rotate-90 whitespace-nowrap absolute left-1/2 -translate-x-1/2'>PROFESSIONAL SKILLS</h1>
+
+      <div className='w-32 border flex flex-col gap-5'>
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-white text-xl font-bold'>HTML</h2>
+          <p className='text-[0.6rem] text-white font-extralight tracking-widest leading-3 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse cum incidunt similique. Totam, aliquam.</p>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-white text-xl font-bold'>HTML</h2>
+          <p className='text-[0.6rem] text-white font-extralight tracking-widest leading-3 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse cum incidunt similique. Totam, aliquam.</p>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <h2 className='text-white text-xl font-bold'>HTML</h2>
+          <p className='text-[0.6rem] text-white font-extralight tracking-widest leading-3 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae esse cum incidunt similique. Totam, aliquam.</p>
+        </div>
+      </div>
+
+
       <div className="w-full absolute bottom-3  flex flex-col justify-center items-center lg:bottom-1">
         <img className="w-[0.6rem] lg:w-[0.9rem]" src={mouse} alt="" />
         <p className="text-center text-white/50 font-thin text-[0.6rem] tracking-widest lg:text-[0.8rem]">
