850c9b1829188e72abba891261c08d568919d5b5
pravesh dav <humantorch1932@gmail.com>
2024-07-01 23:44:09 +0530

    designed right line in skills component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import mouse from "../assets/images/icon/scrollMouse.svg";
 import lineLeftTop from '../assets/images/Skills/lineLeftTop.svg'
 import lineLeftBottom from '../assets/images/Skills/lineLeftBottom.svg'
+import lineRightTop from '../assets/images/Skills/lineRightTop.svg'
+import lineRightBottom from '../assets/images/Skills/lineRightBottom.svg'
 
 function Skills() {
   return (
     <div className="relative">
       {/* here will import decoration */}
 
-      <div className="w-full h-screen relative flex justify-between items-center">
+      <div className="w-80 h-screen relative flex justify-between items-center">
         <div className="w-36 flex flex-col gap-5 px-3 py-14 relative border-r-2 border-primary">
           {/* <img src={lineLeft} className="h-full absolute right-0 top-1/2 -translate-y-1/2" alt="" /> */}
           <img src={lineLeftTop} className="w-[92%] absolute right-0 top-0" alt="" />
@@ -40,7 +42,9 @@ function Skills() {
           PROFESSIONAL SKILLS
         </h1>
 
-        <div className="w-36 border flex flex-col gap-5 px-3 py-4">
+        <div className="w-36 flex flex-col gap-5 px-3 py-14 relative border-l-2 border-primary">
+        <img src={lineRightTop} className="w-[30%] absolute left-0 top-0" alt="" />
+        <img src={lineRightBottom} className="w-[30%] absolute left-0 bottom-0" alt="" />
           <div className="flex flex-col gap-1">
             <h2 className="text-white text-xl font-bold">HTML</h2>
             <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
