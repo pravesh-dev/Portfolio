88ba6c15d8ec4a6fcdd54459220ecfd146b086bb
pravesh dav <humantorch1932@gmail.com>
2024-07-02 13:45:00 +0530

    responsive skills content for largen and medium devices

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,67 +1,74 @@
 import React from "react";
 import mouse from "../assets/images/icon/scrollMouse.svg";
 import lineLeftTop from '../assets/images/Skills/lineLeftTop.svg'
+import lineLeftTopBig from '../assets/images/Skills/lineLeftTopBig.svg'
 import lineLeftBottom from '../assets/images/Skills/lineLeftBottom.svg'
+import lineLeftBottomBig from '../assets/images/Skills/lineLeftBottomBig.svg'
 import lineRightTop from '../assets/images/Skills/lineRightTop.svg'
+import lineRightTopBig from '../assets/images/Skills/lineRightTopBig.svg'
 import lineRightBottom from '../assets/images/Skills/lineRightBottom.svg'
+import lineRightBottomBig from '../assets/images/Skills/lineRightBottomBig.svg'
 
 function Skills() {
   return (
     <div className="relative">
       {/* here will import decoration */}
 
-      <div className="w-80 h-screen relative flex justify-between items-center mx-auto">
-        <div className="w-36 flex flex-col gap-5 px-3 py-14 relative border-r-2 border-primary">
-          {/* <img src={lineLeft} className="h-full absolute right-0 top-1/2 -translate-y-1/2" alt="" /> */}
-          <img src={lineLeftTop} className="w-[92%] absolute right-0 top-0" alt="" />
-          <img src={lineLeftBottom} className="w-[92%] absolute right-0 bottom-0" alt="" />
+      <div className="w-full h-screen relative flex justify-between items-center mx-auto sm:w-[35rem] lg:w-[60rem] xl:w-[75rem]">
+        <div className="w-36 flex flex-col gap-5 px-3 py-14 relative border-r-2 border-primary lg:w-[26rem] lg:py-20 lg:border-r-[2.3px] lg:pr-16">
+          <img src={lineLeftTop} className="w-[92%] absolute right-0 top-0 lg:hidden" alt="" />
+          <img src={lineLeftBottom} className="w-[92%] absolute right-0 bottom-0 lg:hidden" alt="" />
+          <img src={lineLeftTopBig} className="w-[97%] absolute -right-[0.1rem] top-0 hidden lg:block" alt="" />
+          <img src={lineLeftBottomBig} className="w-[97%] absolute -right-[0.1rem] bottom-0 hidden lg:block" alt="" />
           <div className="flex flex-col gap-1">
-            <h2 className="text-white text-xl font-bold">HTML</h2>
-            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
+            <h2 className="text-white text-xl font-bold lg:text-2xl">HTML</h2>
+            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify lg:text-[0.8rem] lg:tracking-[0.18rem] lg:leading-[1rem]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
               esse cum incidunt similique. Totam, aliquam.
             </p>
           </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-white text-xl font-bold">HTML</h2>
-            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
+            <h2 className="text-white text-xl font-bold lg:text-2xl">HTML</h2>
+            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify lg:text-[0.8rem] lg:tracking-[0.18rem] lg:leading-[1rem]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
               esse cum incidunt similique. Totam, aliquam.
             </p>
           </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-white text-xl font-bold">HTML</h2>
-            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
+            <h2 className="text-white text-xl font-bold lg:text-2xl">HTML</h2>
+            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify lg:text-[0.8rem] lg:tracking-[0.18rem] lg:leading-[1rem]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
               esse cum incidunt similique. Totam, aliquam.
             </p>
           </div>
         </div>
 
-        <h1 className="text-primary text-xl font-bold tracking-widest -rotate-90 whitespace-nowrap absolute left-1/2 -translate-x-1/2">
+        <h1 className="text-primary text-xl font-bold tracking-widest -rotate-90 whitespace-nowrap absolute left-1/2 -translate-x-1/2 sm:text-3xl lg:tracking-[0.36rem]">
           PROFESSIONAL SKILLS
         </h1>
 
-        <div className="w-36 flex flex-col gap-5 px-3 py-14 relative border-l-2 border-primary">
-        <img src={lineRightTop} className="w-[30%] absolute left-0 top-0" alt="" />
-        <img src={lineRightBottom} className="w-[30%] absolute left-0 bottom-0" alt="" />
+        <div className="w-36 flex flex-col gap-5 px-3 py-14 relative border-l-2 border-primary lg:w-[26rem] lg:py-20 lg:border-l-[2.3px] lg:pl-16">
+        <img src={lineRightTop} className="w-[30%] absolute left-0 top-0 lg:hidden" alt="" />
+        <img src={lineRightBottom} className="w-[30%] absolute -left-[0.1rem] -bottom-[0.1rem] lg:hidden" alt="" />
+        <img src={lineRightTopBig} className="w-[24%] absolute -left-[0.1rem] top-0 hidden lg:block" alt="" />
+        <img src={lineRightBottomBig} className="w-[24%] absolute -left-[0.1rem] -bottom-[0.1rem] hidden lg:block" alt="" />
           <div className="flex flex-col gap-1">
-            <h2 className="text-white text-xl font-bold">HTML</h2>
-            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
+            <h2 className="text-white text-xl font-bold lg:text-2xl">HTML</h2>
+            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify lg:text-[0.8rem] lg:tracking-[0.18rem] lg:leading-[1rem]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
               esse cum incidunt similique. Totam, aliquam.
             </p>
           </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-white text-xl font-bold">HTML</h2>
-            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
+            <h2 className="text-white text-xl font-bold lg:text-2xl">HTML</h2>
+            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify lg:text-[0.8rem] lg:tracking-[0.18rem] lg:leading-[1rem]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
               esse cum incidunt similique. Totam, aliquam.
             </p>
           </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-white text-xl font-bold">HTML</h2>
-            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify">
+            <h2 className="text-white text-xl font-bold lg:text-2xl">HTML</h2>
+            <p className="text-[0.6rem] text-neutral-300 font-extralight tracking-[0.16rem] leading-[0.8rem] text-justify lg:text-[0.8rem] lg:tracking-[0.18rem] lg:leading-[1rem]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
               esse cum incidunt similique. Totam, aliquam.
             </p>
