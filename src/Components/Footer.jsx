e30f415c8af7b067f576cf8ff4fe8d094059157f
pravesh dav <humantorch1932@gmail.com>
2024-07-03 19:57:47 +0530

    footer responsive for medium devices

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,9 +6,9 @@ import { FaGithub, FaSquareInstagram } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className='w-full h-52 flex flex-col justify-center gap-4 px-2 bg-[#161616]'>
-      <div className='w-full flex text-white flex-col gap-2'>
-        <h2 className='text-white text-[0.9rem]'>Developed By <span className='text-primary font-bold tracking-wider'>Pravesh</span></h2>
+    <div className='w-full h-52 flex flex-col justify-center gap-4 px-2 bg-[#161616] md:h-32'>
+      <div className='w-full flex text-white flex-col gap-2 md:flex-row md:justify-between md:mt-6'>
+        <h2 className='text-white text-[0.9rem] font-yellowTail'>Developed By <span className='text-primary font-bold tracking-wider'>Pravesh</span></h2>
         <a href="tel:+91 9991831473" className='text-[0.8rem] flex items-center gap-1'><span className='text-primary text-[1.2rem]'><IoIosCall/></span> +91 9991831473</a>
         <h3 className='text-[0.8rem] flex items-center gap-1'><span className='text-primary text-[1.2rem]'><IoIosPin/></span> 84/3 Niwaz Nagar, Narnaul, Haryana</h3>
         <div className='flex gap-1'>
@@ -19,7 +19,7 @@ function Footer() {
         </div>
       </div>
       <img src={line} className='w-full' alt="" />
-      <div className='w-full h-5 text-white text-xs flex items-center justify-center uppercase'>© <span className='text-primary mx-1 font-bold'>Pravesh</span> 2024. ALL rIGHTS rESERVED</div>
+      <div className='w-full h-5 text-white text-xs flex items-center justify-center uppercase lg:text-sm lg:tracking-widest'>© <span className='text-primary mx-1 font-bold'>Pravesh</span> 2024. ALL rIGHTS rESERVED</div>
     </div>
   )
 }
