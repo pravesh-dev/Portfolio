27ae720bc2df59b4eb1ef4e01a5fd9e7405e9330
pravesh dav <humantorch1932@gmail.com>
2024-07-03 18:56:41 +0530

    imported icons from react-icons in footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import line from  '../assets/images/footerLine.svg'
+import line from  '../assets/images/footerLine.svg';
+import { IoIosCall, IoIosPin, IoMdMail } from "react-icons/io";
+import { FaLinkedin, FaRegCopyright } from "react-icons/fa";
+import { FaGithub, FaSquareInstagram } from "react-icons/fa6";
+
 function Footer() {
   return (
     <div className='w-full h-32 border flex flex-col justify-center gap-1'>
       <div className='w-full h-10 bg-red-800'>
         <h2 className='text-white'>Developed By <span className='text-primary'>Pravesh</span></h2>
+        <a href="tel:+91 9991831473">+91 9991831473</a>
       </div>
       <img src={line} className='w-full' alt="" />
       <div className='w-full h-5 bg-blue-600'></div>
