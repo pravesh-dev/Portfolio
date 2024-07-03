59220683f1a48ca46b30ef7375fe8a19caa31bc6
pravesh dav <humantorch1932@gmail.com>
2024-07-03 20:05:44 +0530

    responsive footer for large devices

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,19 +6,19 @@ import { FaGithub, FaSquareInstagram } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <div className='w-full h-52 flex flex-col justify-center gap-4 px-2 bg-[#161616] md:h-32'>
-      <div className='w-full flex text-white flex-col gap-2 md:flex-row md:justify-between md:mt-6'>
-        <h2 className='text-white text-[0.9rem] font-yellowTail'>Developed By <span className='text-primary font-bold tracking-wider'>Pravesh</span></h2>
-        <a href="tel:+91 9991831473" className='text-[0.8rem] flex items-center gap-1'><span className='text-primary text-[1.2rem]'><IoIosCall/></span> +91 9991831473</a>
-        <h3 className='text-[0.8rem] flex items-center gap-1'><span className='text-primary text-[1.2rem]'><IoIosPin/></span> 84/3 Niwaz Nagar, Narnaul, Haryana</h3>
-        <div className='flex gap-1'>
-          <a className='text-[1rem]' href="#"><FaLinkedin/></a>
-          <a className='text-[1rem]' href="#"><FaGithub /></a>
-          <a className='text-[1rem]' href="#"><FaSquareInstagram /></a>
-          <a className='text-[1rem]' href="#"><IoMdMail /></a>
+    <div className='w-full h-52 flex flex-col justify-center gap-4 px-2 bg-[#161616] md:h-32 lg:h-40'>
+      <div className='w-full flex text-white flex-col gap-2 md:flex-row md:justify-between md:mt-6 lg:px-14 xl:px-32'>
+        <h2 className='text-white text-[0.9rem] font-yellowTail lg:text-2xl'>Developed By <span className='text-primary font-bold tracking-widest font-yellowTail'>Pravesh</span></h2>
+        <a href="tel:+91 9991831473" className='text-[0.8rem] flex items-center gap-1 lg:text-[0.9rem]'><span className='text-primary text-[1.2rem] lg:text-3xl'><IoIosCall/></span> +91 9991831473</a>
+        <h3 className='text-[0.8rem] flex items-center gap-1 lg:text-[0.9rem]'><span className='text-primary text-[1.2rem] lg:text-3xl'><IoIosPin/></span> 84/3 Niwaz Nagar, Narnaul, Haryana</h3>
+        <div className='flex gap-1 lg:gap-2'>
+          <a className='text-[1rem] lg:text-xl' href="#"><FaLinkedin/></a>
+          <a className='text-[1rem] lg:text-xl' href="#"><FaGithub /></a>
+          <a className='text-[1rem] lg:text-xl' href="#"><FaSquareInstagram /></a>
+          <a className='text-[1rem] lg:text-xl' href="#"><IoMdMail /></a>
         </div>
       </div>
-      <img src={line} className='w-full' alt="" />
+      <img src={line} className='w-full lg:w-[95%] lg:mx-auto' alt="" />
       <div className='w-full h-5 text-white text-xs flex items-center justify-center uppercase lg:text-sm lg:tracking-widest'>© <span className='text-primary mx-1 font-bold'>Pravesh</span> 2024. ALL rIGHTS rESERVED</div>
     </div>
   )
