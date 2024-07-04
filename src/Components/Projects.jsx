d42adb6251a4f69ad6e6f3dd0648af1bac18a09d
pravesh dav <humantorch1932@gmail.com>
2024-07-04 19:09:46 +0530

    added created project card and added  content

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import triangle from '../assets/images/Projects/triangle.svg'
+import img from '../assets/images/Projects/img.svg'
 
 function Projects() {
     const [activeTab, setActiveTab] = useState('all');
@@ -13,6 +14,14 @@ function Projects() {
         <button className={`text-sm font-semibold px-3 py-1 rounded-lg ${activeTab === 'all' ? 'bg-[#646464] text-primary' : 'text-neutral-300/70'}`} onClick={()=>{handleTabClick('all')}}>ALL</button>
         <button className={`text-sm font-semibold px-3 py-1 rounded-lg ${activeTab === 'frontend' ? 'bg-[#646464] text-primary' : 'text-neutral-300/70'}`} onClick={()=>{handleTabClick('frontend')}}>Frontend</button>
         <button className={`text-sm font-semibold px-3 py-1 rounded-lg ${activeTab === 'full' ? 'bg-[#646464] text-primary' : 'text-neutral-300/70'}`} onClick={()=>{handleTabClick('full')}}>Full Stack</button>
+      </div>
+      <div className='w-full flex flex-wrap gap-3 px-2'>
+        <div className='w-36 h-52 bg-[#aaaaaa29] p-[0.33rem]'>
+            <h2 className='text-white text-xs font-semibold'>GolfClub Company Website Frontend Clone</h2>
+            <p className='text-neutral-500 text-[0.5rem] text-justify'>"It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.</p>
+            <a href="#">Visit Website <span className='text-primary'>→</span></a>
+            <img src={img} alt="" />
+        </div>
       </div>
     </div>
   )
