ce1292202056d676f972d7262ec9424ee9aa03e7
pravesh dav <humantorch1932@gmail.com>
2024-07-08 16:09:36 +0530

    responsive project cards

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -27,18 +27,18 @@ function Projects() {
       : filteredProjects.slice(0, 6);
 
     return displayedProjects.map((project, index) => (
-      <div key={index} className="w-36 bg-[#aaaaaa29] p-[0.33rem] rounded-md">
-        <h2 className="text-white text-[0.7rem] font-semibold">
+      <div key={index} className="w-36 bg-[#aaaaaa29] p-[0.33rem] rounded-md sm:w-56 sm:flex flex-col gap-2 sm:p-3 lg:w-64">
+        <h2 className="text-white text-[0.7rem] font-semibold sm:text-[1rem] lg:text-[1.3rem]">
           {project.name}
         </h2>
-        <p className="text-neutral-500 text-[0.5rem] text-justify">
+        <p className="text-neutral-500 text-[0.5rem] text-justify sm:text-[0.65rem] lg:text-[0.8rem]">
           {project.para}
         </p>
         <a
           href={project.link}
-          className="text-white text-[0.6rem] flex items-center gap-1"
+          className="text-white text-[0.6rem] flex items-center gap-1 sm:text-[0.9rem] lg:text-[1.2rem]"
         >
-          Visit Website <span className="text-primary text-xl">→</span>
+          Visit Website <span className="text-primary text-xl sm:text-[1.5rem] lg:text-[2.25rem] lg:-mt-1">→</span>
         </a>
         <img src={project.image} className="rounded-lg" alt={project.name} />
       </div>
@@ -46,8 +46,8 @@ function Projects() {
   };
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center py-8 gap-8 relative">
-      <h1 className="text-primary text-center w-40 text-2xl font-semibold relative pb-1 uppercase">
+    <div className="w-full min-h-screen flex flex-col items-center gap-8 relative p-16">
+      <h1 className="text-primary text-center w-40 text-2xl font-semibold relative pb-1 uppercase lg:text-4xl lg:w-56">
         Projects
         <span className="w-full h-[0.1rem] bg-primary absolute left-0 bottom-0">
           <img
@@ -60,7 +60,7 @@ function Projects() {
           />
         </span>
       </h1>
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-3 items-center lg:mb-7">
         {["all", "frontend", "backend"].map((tab) => (
           <button
             key={tab}
@@ -68,14 +68,14 @@ function Projects() {
               activeTab === tab
                 ? "bg-[#646464] text-primary"
                 : "text-neutral-300/70"
-            }`}
+            } lg:text-lg`}
             onClick={() => handleTabClick(tab)}
           >
             {tab}
           </button>
         ))}
       </div>
-      <div className="w-80 flex-wrap gap-3 px-2 mx-auto flex">
+      <div className="w-80 flex-wrap gap-3 px-2 flex sm:w-[31.5rem] sm:gap-8 lg:w-[56.5rem] lg:gap-14 xl:w-[60rem] xl:gap-20 border">
         {renderProjects(activeTab)}
       </div>
       <button
