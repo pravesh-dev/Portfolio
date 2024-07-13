ba2f2387b445d2e52025128f10c2ce79799a9844
pravesh dav <humantorch1932@gmail.com>
2024-07-13 12:47:32 +0530

    defined routing in app.jsx and created a layout.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import Header from './Components/Header'
-import Hero from './Components/Hero'
-import About from './Components/About'
-import Skills from './Components/Skills'
-import Footer from './Components/Footer'
-import Projects from './Components/Projects'
-import Resume from './Components/Resume'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Footer from "./Components/Footer";
+import Projects from "./Components/Projects";
+import Resume from "./Components/Resume";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Resume/>
-      <Footer/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={
+              <>
+                <Hero /> <About /> <Skills /> <Projects /> <Resume />{" "}
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+          <Route path="/resume" element={<Resume/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
