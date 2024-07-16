97aa3b096c139046dcb4c5e1f68f896d8448a65a
pravesh dav <humantorch1932@gmail.com>
2024-07-16 13:46:19 +0530

    added projects images and imported

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -1,12 +1,17 @@
 import img from "../assets/images/Projects/img.svg";
+import hexadefend from "../assets/images/Projects/hexadefend.svg";
+import carva from "../assets/images/Projects/carva.svg";
+import criativo from "../assets/images/Projects/criativo.svg";
+import rejouice from "../assets/images/Projects/rejouice.svg";
+import stepMatter from "../assets/images/Projects/step-matter.svg";
 
 
 
 const projects = [
     {
-      name: "GolfClub Company Website UI Clone",
+      name: "Hexadefend",
       para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
-      link: '#',
+      link: 'https://www.hexadefend.com/',
       image: img,
       type: 'frontend'
     },
