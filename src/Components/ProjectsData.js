ea009dd73c96b0fa3fc8b32ae5ec53473bf6f96c
pravesh dav <humantorch1932@gmail.com>
2024-07-16 13:57:49 +0530

    added projects image, name and link

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -4,50 +4,51 @@ import carva from "../assets/images/Projects/carva.svg";
 import criativo from "../assets/images/Projects/criativo.svg";
 import rejouice from "../assets/images/Projects/rejouice.svg";
 import stepMatter from "../assets/images/Projects/step-matter.svg";
+import fanta from "../assets/images/Projects/fanta.png";
 
 
 
 const projects = [
     {
-      name: "Hexadefend",
+      name: "Hexadefend UI Development",
       para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
       link: 'https://www.hexadefend.com/',
-      image: img,
+      image: hexadefend,
       type: 'frontend'
     },
     {
-      name: "GolfClub Company Website UI Clone",
+      name: "Carva Design & Development",
       para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
-      link: '#',
-      image: img,
-      type: 'backend'
-    },
-    {
-      name: "GolfClub Company Website UI Clone",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
-      link: '#',
-      image: img,
-      type: 'backend'
-    },
-    {
-      name: "GolfClub Company Website UI Clone",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
-      link: '#',
-      image: img,
+      link: 'https://carva-edb5c.web.app/',
+      image: carva,
       type: 'frontend'
     },
     {
-      name: "GolfClub Company Website UI Clone",
+      name: "Step Matter Design & Development",
       para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
-      link: '#',
-      image: img,
+      link: 'https://step-matters.web.app/',
+      image: stepMatter,
       type: 'frontend'
     },
     {
-      name: "GolfClub Company Website UI Clone",
+      name: "Criativo UI Development",
       para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
-      link: '#',
-      image: img,
+      link: 'https://criativo-112.web.app/',
+      image: criativo,
+      type: 'frontend'
+    },
+    {
+      name: "Rejouice Website UI Clone",
+      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      link: 'https://rejouice-25.web.app/',
+      image: rejouice,
+      type: 'frontend'
+    },
+    {
+      name: "Fanta Design Clone",
+      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      link: 'https://fanta-2024.web.app/',
+      image: fanta,
       type: 'frontend'
     },
   ]
