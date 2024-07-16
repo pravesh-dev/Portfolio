322590291fe5504f978c3d99a12c924dfe6ce312
pravesh dav <humantorch1932@gmail.com>
2024-07-16 14:14:28 +0530

    added text paragraph for projects

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -6,51 +6,49 @@ import rejouice from "../assets/images/Projects/rejouice.svg";
 import stepMatter from "../assets/images/Projects/step-matter.svg";
 import fanta from "../assets/images/Projects/fanta.png";
 
-
-
 const projects = [
     {
       name: "Hexadefend UI Development",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      para: `Hexadefend is the startup where I worked as a frontend web developer. I was responsible for developing their website, a comprehensive project involving multiple pages, functionalities, and deployment. It was built using React and Tailwind CSS.`,
       link: 'https://www.hexadefend.com/',
       image: hexadefend,
       type: 'frontend'
     },
     {
       name: "Carva Design & Development",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      para: `Carva's website was designed and developed with inspiration from various sources. I created the landing page UI using HTML, CSS, JavaScript, and GSAP.`,
       link: 'https://carva-edb5c.web.app/',
       image: carva,
       type: 'frontend'
     },
     {
       name: "Step Matter Design & Development",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      para: `Step Matter's landing page UI was crafted using HTML, CSS, JavaScript, and GSAP, drawing inspiration from effective design practices.`,
       link: 'https://step-matters.web.app/',
       image: stepMatter,
       type: 'frontend'
     },
     {
       name: "Criativo UI Development",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      para: `Criativo's landing page UI was based on a design found on Pinterest. I implemented it using HTML, CSS, JavaScript, and GSAP.`,
       link: 'https://criativo-112.web.app/',
       image: criativo,
       type: 'frontend'
     },
     {
       name: "Rejouice Website UI Clone",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      para: `Rejouice's landing page UI was cloned using HTML, CSS, JavaScript, and GSAP, replicating its original design and functionality.`,
       link: 'https://rejouice-25.web.app/',
       image: rejouice,
       type: 'frontend'
     },
     {
       name: "Fanta Design Clone",
-      para: `It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were in a dream I had just a couple of days ago.`,
+      para: `Fanta's landing page UI clone was developed to showcase its popular design and smooth animations, created with HTML, CSS, JavaScript, and GSAP.`,
       link: 'https://fanta-2024.web.app/',
       image: fanta,
       type: 'frontend'
     },
-  ]
+  ];
 
-export {projects}
+export { projects };
