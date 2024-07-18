03e359883b472e569736e16f18281c444677e7ed
pravesh dav <humantorch1932@gmail.com>
2024-07-18 18:44:47 +0530

    added social media links in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,10 +12,10 @@ function Footer() {
         <a href="tel:+91 9991831473" className='text-[0.8rem] flex items-center gap-1 lg:text-[0.9rem] lg:tracking-wider'><span className='text-primary text-[1.2rem] lg:text-3xl'><IoIosCall/></span> +91 9991831473</a>
         <h3 className='text-[0.8rem] flex items-center gap-1 lg:text-[0.9rem] lg:tracking-wider'><span className='text-primary text-[1.2rem] lg:text-3xl'><IoIosPin/></span> 84/3 Niwaz Nagar, Narnaul, Haryana</h3>
         <div className='flex gap-1 lg:gap-2'>
-          <a className='text-[1rem] lg:text-xl' href="#"><FaLinkedin/></a>
-          <a className='text-[1rem] lg:text-xl' href="#"><FaGithub /></a>
-          <a className='text-[1rem] lg:text-xl' href="#"><FaSquareInstagram /></a>
-          <a className='text-[1rem] lg:text-xl' href="#"><IoMdMail /></a>
+          <a href="https://www.linkedin.com/in/pravesh-saini-b7ab39280/" target="_blank" rel="noopener noreferrer" className='text-[1rem] lg:text-xl'><FaLinkedin/></a>
+          <a href="https://github.com/pravesh-dev" target="_blank" rel="noopener noreferrer" className='text-[1rem] lg:text-xl'><FaGithub /></a>
+          <a href="https://www.instagram.com/dev_pravesh/" target="_blank" rel="noopener noreferrer" className='text-[1rem] lg:text-xl'><FaSquareInstagram /></a>
+          <a href="mailto:praveshsaini.dev@gmail.com" className='text-[1rem] lg:text-xl'><IoMdMail /></a>
         </div>
       </div>
       <img src={line} className='w-full lg:w-[95%] lg:mx-auto' alt="" />
