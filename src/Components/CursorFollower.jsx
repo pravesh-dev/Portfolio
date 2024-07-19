3a0c8d02e1e54cefd4fa31501973df947c4dd56d
pravesh dav <humantorch1932@gmail.com>
2024-07-19 22:25:48 +0530

    added cursor follower

--- src/Components/CursorFollower.jsx
+++ src/Components/CursorFollower.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function CursorFollower() {
+  const [cursorPosition, setCursorPosition] = useState({top: 200, left: 100})
+
+  useEffect(() => {
+    const handleMouseMove = (e)=>{
+      setCursorPosition({top: e.clientY, left: e.clientX});
+    }
+  
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    }
+  }, [])
+  
   return (
     <div>
-      
+      <div className={`w-16 h-16 rounded-full border border-neutral-400 shadow-[0_0_7px_white] fixed -translate-x-1/2 -translate-y-1/2 `} style={{ top: `${cursorPosition.top}px`, left: `${cursorPosition.left}px` }}></div>
+      <div className={`w-28 h-28 rounded-full border border-neutral-400 shadow-[0_0_7px_white] fixed -translate-x-1/2 -translate-y-1/2 `} style={{ top: `${cursorPosition.top}px`, left: `${cursorPosition.left}px` }}></div>
     </div>
   )
 }
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
+import CursorFollower from './CursorFollower'
 function Layout() {
   return (
     <main>
       <Header/>
+      <CursorFollower/>
       <Outlet/>
     </main>
   )
