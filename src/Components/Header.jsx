482f22363a91c6663bea93fc17012cf8e1a6c5ab
pravesh dav <humantorch1932@gmail.com>
2024-07-20 21:22:07 +0530

    created header.css file and added new hover in nav items and new active item effect

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import bar from "../assets/images/icon/bar.svg";
 import { Link } from "react-router-dom";
+import './Header.css'
 
 function Header() {
   const [activeNav, setActiveNav] = useState(false);
@@ -14,10 +15,10 @@ function Header() {
         <Link to='/' className="font-yellowTail text-white text-xl lg:text-2xl">Pravesh</Link>
         <img loading="lazy" src={bar} onClick={handleNav} className="cursor-pointer md:hidden" alt="" />
         <nav className="gap-5 lg:gap-12 hidden md:flex">
-          <Link to='/' className={`text-xs font-extralight tracking-wider ${activeTab === 'home' ? 'text-neutral-400' : 'text-white'} duration-500 hover:scale-105 cursor_pointer`} onClick={()=>{setActiveTab('home')}}>Home</Link>
-          <Link to='/about' className={`text-xs font-extralight tracking-wider ${activeTab === 'about' ? 'text-neutral-400' : 'text-white'} duration-500 hover:scale-105 cursor_pointer`} onClick={()=>{setActiveTab('about')}}>About</Link>
-          <Link to='/projects' className={`text-xs font-extralight tracking-wider ${activeTab === 'project' ? 'text-neutral-400' : 'text-white'} duration-500 hover:scale-105 cursor_pointer`} onClick={()=>{setActiveTab('project')}}>Projects</Link>
-          <Link to='resume' className={`text-xs font-extralight tracking-wider ${activeTab === 'resume' ? 'text-neutral-400' : 'text-white'} duration-500 hover:scale-105 cursor_pointer`} onClick={()=>{setActiveTab('resume')}}>Resume</Link>
+          <Link to='/' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'home' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('home')}}>Home</Link>
+          <Link to='/about' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'about' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('about')}}>About</Link>
+          <Link to='/projects' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'projects' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('projects')}}>Projects</Link>
+          <Link to='resume' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'resume' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('resume')}}>Resume</Link>
         </nav>
         <a href="mailto:praveshsaini.dev@gmail.com" className="bg-primary text-white rounded-[1rem] text-sm px-3 py-1 hidden md:block shadow-md shadow-white/80 lg:px-4 hover:translate-y-1 hover:shadow-none duration-500 active:duration-100 active:scale-90 cursor_pointer" aria-label="Send an email to Pravesh">Let's Talk</a>
       </header>
