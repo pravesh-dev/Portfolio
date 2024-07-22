b1c57dae46fb1b89ececa547860c7967615329a1
pravesh dav <humantorch1932@gmail.com>
2024-07-22 21:11:48 +0530

    added mobile nav content

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import bar from "../assets/images/icon/bar.svg";
 import { Link } from "react-router-dom";
 import './Header.css'
+import { IoClose } from "react-icons/io5";
 
 function Header() {
   const [activeNav, setActiveNav] = useState(false);
@@ -23,10 +24,21 @@ function Header() {
         <a href="mailto:praveshsaini.dev@gmail.com" className="bg-primary text-white rounded-[1rem] text-sm px-3 py-1 hidden md:block shadow-md shadow-white/80 lg:px-4 hover:translate-y-1 hover:shadow-none duration-500 active:duration-100 active:scale-90 cursor_pointer" aria-label="Send an email to Pravesh">Let's Talk</a>
       </header>
       <div
-        className={`w-[80%] h-screen bg-red-500 fixed z-[100] ${
+        className={`w-full h-screen bg-[#111] fixed z-[100] ${
           activeNav ? "block" : "hidden"
         }`}
-      ></div>
+      >
+        <div>
+        <Link to='/' className="font-yellowTail text-white text-xl lg:text-2xl">Pravesh</Link>
+        <span><IoClose/></span>
+        </div>
+        <div>
+        <Link to='/' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'home' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('home')}}>Home</Link>
+          <Link to='/about' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'about' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('about')}}>About</Link>
+          <Link to='/projects' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'projects' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('projects')}}>Projects</Link>
+          <Link to='resume' className={`nav_items text-xs font-normal tracking-wider lg:text-base cursor_pointer ${activeTab === 'resume' ? 'bg-[#ff2403]' : ''}`} onClick={()=>{setActiveTab('resume')}}>Resume</Link>
+        </div>
+      </div>
     </>
   );
 }
