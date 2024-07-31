274b29ecf157a8ebd5b512b28f7af02edca94458
pravesh dav <humantorch1932@gmail.com>
2024-07-31 20:40:44 +0530

    animating cursor follower using gsap

--- src/Components/CursorFollower.jsx
+++ src/Components/CursorFollower.jsx
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 
 function CursorFollower() {
-  const [cursorPosition, setCursorPosition] = useState({top: 200, left: 100})
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleMouseMove = (e)=>{
-      setCursorPosition({top: e.clientY, left: e.clientX});
-    }
-  
-    window.addEventListener("mousemove", handleMouseMove);
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      setMousePosition({ x: clientX, y: clientY });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
     return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    }
-  }, [])
-  
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+
+    gsap.to('.cursor_Circs', {
+      duration: 0.1,
+      x: mousePosition.x,
+      y: mousePosition.y,
+      ease: "power2.out",
+    });
+  }, [mousePosition]);
+
   return (
-    <div className='pointer-events-none'>
-      <div className={`w-1 h-1 rounded-full bg-[#fd563c0f] border-[0.5px] border-white/20 fixed -translate-x-1/2 -translate-y-1/2 duration-100`} style={{ top: `${cursorPosition.top}px`, left: `${cursorPosition.left}px` }}></div>
-      <div className={`w-8 h-8 rounded-full  border-[0.5px] border-white/20 fixed -translate-x-1/2 -translate-y-1/2 duration-100`} style={{ top: `${cursorPosition.top}px`, left: `${cursorPosition.left}px` }}></div>
-      {/* <div className={`w-[27rem] h-[27rem] rounded-full bg-[#ff30101a] filter blur-2xl fixed -translate-x-1/2 -translate-y-1/2 duration-100`} style={{ top: `${cursorPosition.top}px`, left: `${cursorPosition.left}px` }}></div> */}
+    <div className="pointer-events-none">
+      <div
+        className={`cursor_Circs w-1 h-1 rounded-full bg-[#fd563c0f] border-[0.5px] border-white/20 fixed -translate-x-1/2 -translate-y-1/2 duration-100`}></div>
+      <div
+        className={`cursor_Circs w-8 h-8 rounded-full  border-[0.5px] border-white/20 fixed -translate-x-1/2 -translate-y-1/2 duration-100`}></div>
+      {/* <div className={`cursor_Circs w-[27rem] h-[27rem] rounded-full bg-[#ff30101a] filter blur-2xl fixed -translate-x-1/2 -translate-y-1/2 duration-100`} style={{ top: `${cursorPosition.top}px`, left: `${cursorPosition.left}px` }}></div> */}
     </div>
-  )
+  );
 }
 
-export default CursorFollower
+export default CursorFollower;
